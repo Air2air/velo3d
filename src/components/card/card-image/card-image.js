520d9dc47591dba46da81e620ceab4ac407b0c92
Air2air <todd@dunningmarketing.com>
2021-06-14 10:56:06 -0700

    more

--- src/components/card/card-image/card-image.js
+++ src/components/card/card-image/card-image.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./styles.scss";
 
+const imgPath = (image) => {
+  const newPath = require(`./../../../images/${image}.jpg`).default;
+  return newPath;
+};
+
 function CardImage(props) {
   return (
     <div
       className="card-image p-3 d-flex flex-column justify-content-end"
-      style={{ backgroundImage: `url(${props.image})` }}
+      style={{ backgroundImage: `url(${imgPath(props.image)})` }}
     >
       <div className="image-meta d-flex justify-content-between">
         <div className="image-legend">Layer 1175/2342</div>
